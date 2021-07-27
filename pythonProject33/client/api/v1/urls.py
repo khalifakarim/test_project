899ce6336aae644c93c30f52b92cce0d542f351c
KarimKkhalifa <kkhalifakarim@gmail.com>
2021-07-27 17:07:00 +0300

    corrected comments

--- pythonProject33/client/api/v1/urls.py
+++ pythonProject33/client/api/v1/urls.py
@@ -19,6 +19,6 @@ router.register("update", UserUpdateViewSet)
 router.register("offer", OfferViewSet)
 
 urlpatterns = [
-                  path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-                  path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-              ] + router.urls
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+] + router.urls
